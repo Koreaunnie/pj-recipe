12263c2711ca4fbd9fc6f98f93cd651d92175718
koreaunnie <hyunjung1995@gmail.com>
2024-10-23 17:37:51 +0900

    First commit

---
+++ src/test/java/com/example/pjrecipe/PjRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pjrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
